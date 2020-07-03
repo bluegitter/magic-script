a870c74cea3be6dff6c6ea52738c56219a5f1566
mxd <838425805@qq.com>
2020-07-03 19:27:38 +0800

    map $$

--- src/main/java/org/ssssssss/script/parsing/ast/MapLiteral.java
+++ src/main/java/org/ssssssss/script/parsing/ast/MapLiteral.java
@@ -1,6 +1,7 @@
 package org.ssssssss.script.parsing.ast;
 
 import org.ssssssss.script.MagicScriptContext;
+import org.ssssssss.script.MagicScriptError;
 import org.ssssssss.script.parsing.Span;
 import org.ssssssss.script.parsing.Token;
 import org.ssssssss.script.parsing.TokenType;
@@ -10,34 +11,37 @@ import java.util.List;
 import java.util.Map;
 
 public class MapLiteral extends Literal {
-    private final List<Token> keys;
-    private final List<Expression> values;
+	private final List<Token> keys;
+	private final List<Expression> values;
 
-    public MapLiteral(Span span, List<Token> keys, List<Expression> values) {
-        super(span);
-        this.keys = keys;
-        this.values = values;
-    }
+	public MapLiteral(Span span, List<Token> keys, List<Expression> values) {
+		super(span);
+		this.keys = keys;
+		this.values = values;
+	}
 
-    @Override
-    public Object evaluate(MagicScriptContext context) {
-        Map<String, Object> map = new HashMap<>();
-        for (int i = 0, n = keys.size(); i < n; i++) {
-            Object value = values.get(i).evaluate(context);
-            Token tokenKey = keys.get(i);
-            String key = tokenKey.getSpan().getText();
-            if (tokenKey.getType() == TokenType.StringLiteral) {
-                key = (String) new StringLiteral(tokenKey.getSpan()).evaluate(context);
-            } else if (key != null && key.startsWith("$")) {
-                Object objKey = context.get(key.substring(1));
-                if (objKey != null) {
-                    key = objKey.toString();
-                } else {
-                    key = null;
-                }
-            }
-            map.put(key, value);
-        }
-        return map;
-    }
+	@Override
+	public Object evaluate(MagicScriptContext context) {
+		Map<String, Object> map = new HashMap<>();
+		for (int i = 0, n = keys.size(); i < n; i++) {
+			Object value = values.get(i).evaluate(context);
+			Token tokenKey = keys.get(i);
+			String key = tokenKey.getSpan().getText();
+			if (tokenKey.getType() == TokenType.StringLiteral) {
+				key = (String) new StringLiteral(tokenKey.getSpan()).evaluate(context);
+			} else if (key != null && key.startsWith("$")) {
+				key = key.substring(1);
+				if (!key.startsWith("$")) {
+					Object objKey = context.get(key);
+					if (objKey != null) {
+						key = objKey.toString();
+					} else {
+						MagicScriptError.error("map key is null", tokenKey.getSpan());
+					}
+				}
+			}
+			map.put(key, value);
+		}
+		return map;
+	}
 }

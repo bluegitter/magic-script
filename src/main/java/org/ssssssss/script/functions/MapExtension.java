31d5e378be9e26403a01fdba4435c05af63f865f
mxd <838425805@qq.com>
2020-11-21 14:27:04 +0800

    `Map`增加`each`、`sort`、`merge`、`asList`、`asString`函数

--- src/main/java/org/ssssssss/script/functions/MapExtension.java
+++ src/main/java/org/ssssssss/script/functions/MapExtension.java
@@ -1,15 +1,15 @@
 package org.ssssssss.script.functions;
 
 import org.ssssssss.script.annotation.Comment;
+import org.ssssssss.script.parsing.ast.binary.LessOperation;
 import org.ssssssss.script.reflection.AbstractReflection;
 import org.ssssssss.script.reflection.JavaReflection;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
-import java.util.Map;
-import java.util.Objects;
-import java.util.Set;
+import java.util.*;
+import java.util.function.Function;
 
 public class MapExtension {
 
@@ -30,10 +30,96 @@ public class MapExtension {
 				}
 			}
 		} catch (InstantiationException | IllegalAccessException ignored) {
-			ignored.printStackTrace();
 		}
 		return result;
 	}
+
+	@Comment("循环Map")
+	public static Map<?, ?> each(Map<?, ?> source, @Comment("循环函数，如:(key,value,source)=>map['xx'] = key;") Function<Object[], Object> function) {
+		source.forEach((key, value) -> function.apply(new Object[]{key, value, source}));
+		return source;
+	}
+
+	@Comment("map转List")
+	public static List<?> asList(Map<?, ?> source, @Comment("映射函数，如:(key,value,source)=>map['xx'] = key;") Function<Object[], Object> mapping) {
+		List<Object> result = new ArrayList<>();
+		source.forEach((key, value) -> result.add(mapping.apply(new Object[]{key, value, source})));
+		return result;
+	}
+
+	@Comment("合并Map")
+	public static Map<?, ?> merge(Map<Object, Object> source, @Comment("key") Object key, @Comment("value") Object value) {
+		source.put(key, value);
+		return source;
+	}
+
+	@Comment("合并Map")
+	public static Map<?, ?> merge(Map<Object, Object> source, @Comment("另一个map或多个Map") Map<Object, Object>... targets) {
+		if (targets != null) {
+			for (int i = 0, len = targets.length; i < len; i++) {
+				source.putAll(targets[i]);
+			}
+		}
+		return source;
+	}
+
+	@Comment("将Map转为String")
+	public static String asString(Map<?, ?> source, @Comment("key与key之间的连接符如&") String separator, @Comment("key与value之间的连接符，如=") String join) {
+		Set<? extends Map.Entry<?, ?>> entries = source.entrySet();
+		StringBuilder builder = new StringBuilder();
+		for (Map.Entry<?, ?> entry : entries) {
+			builder.append(entry.getKey());
+			builder.append(join);
+			builder.append(Objects.toString(entry.getValue(), ""));
+			builder.append(separator);
+		}
+		if (entries.size() > 0) {
+			return builder.substring(0, builder.length() - separator.length());
+		}
+		return builder.toString();
+	}
+
+	@Comment("将Map转为String")
+	public static String asString(Map<?, ?> source, @Comment("key与value之间的连接符，如=") String separator,
+								  @Comment("转换方法，如：(key,value)=>key + '=' + value || ''") Function<Object[], Object> mapping) {
+		Set<? extends Map.Entry<?, ?>> entries = source.entrySet();
+		StringBuilder builder = new StringBuilder();
+		for (Map.Entry<?, ?> entry : entries) {
+			builder.append(Objects.toString(mapping.apply(new Object[]{entry.getKey(), entry.getValue()}), ""));
+			builder.append(separator);
+		}
+		if (entries.size() > 0) {
+			return builder.substring(0, builder.length() - separator.length());
+		}
+		return builder.toString();
+	}
+
+	@Comment("对Map进行排序")
+	public static Map<?, ?> sort(Map<?, ?> source) {
+		Set<?> keys = source.keySet();
+		Map<Object, Object> sortedMap = new LinkedHashMap<>();
+		keys.stream().sorted((Comparator<Object>) (o1, o2) -> {
+			if (Objects.equals(o1, o2)) {
+				return 0;
+			}
+			int value = LessOperation.compare(o1, o2);
+			return value == -2 ? 0 : value;
+		}).forEach(key -> {
+			sortedMap.put(key, source.get(key));
+		});
+		return sortedMap;
+	}
+
+	@Comment("对Map进行排序")
+	public static Map<?, ?> sort(Map<?, ?> source, @Comment("比较器，如:(k1,k2)=>k1.compareTo(k2);") Function<Object[], Object> comparator) {
+		Set<?> keys = source.keySet();
+		Map<Object, Object> sortedMap = new LinkedHashMap<>();
+		keys.stream().sorted((Comparator<Object>) (o1, o2) -> ObjectConvertExtension.asInt(comparator.apply(new Object[]{o1, o2}), 0)).forEach(key -> {
+			sortedMap.put(key, source.get(key));
+		});
+		return sortedMap;
+	}
+
 
 	private static void setFieldValue(Object object, Field field, Object value) {
 		if (field != null) {
@@ -50,7 +136,6 @@ public class MapExtension {
 					reflection.setFieldValue(object, field, value);
 				}
 			} catch (Exception ignored) {
-				ignored.printStackTrace();
 			}
 		}
 	}

b3f5ce651c57d832bd74b5e6127a30db42bf36d9
mxd <838425805@qq.com>
2021-07-11 17:30:25 +0800

    fix BigDecimal

--- src/main/java/org/ssssssss/script/parsing/ast/literal/BigDecimalLiteral.java
+++ src/main/java/org/ssssssss/script/parsing/ast/literal/BigDecimalLiteral.java
@@ -1,5 +1,6 @@
 package org.ssssssss.script.parsing.ast.literal;
 
+import org.ssssssss.script.compile.MagicScriptCompiler;
 import org.ssssssss.script.parsing.Span;
 import org.ssssssss.script.parsing.ast.Literal;
 
@@ -10,7 +11,14 @@ import java.math.BigDecimal;
  */
 public class BigDecimalLiteral extends Literal {
 	public BigDecimalLiteral(Span literal) {
-		super(literal, new BigDecimal(literal.getText().substring(0, literal.getText().length() - 1)));
+		super(literal, literal.getText().substring(0, literal.getText().length() - 1));
 	}
 
+	@Override
+	public void compile(MagicScriptCompiler compiler) {
+		compiler.typeInsn(NEW, BigDecimal.class)
+				.insn(DUP)
+				.ldc(value)
+				.invoke(INVOKESPECIAL, BigDecimal.class, "<init>", void.class, String.class);
+	}
 }

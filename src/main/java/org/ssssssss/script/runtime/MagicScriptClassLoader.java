abb1a24de75514019071c5227d5a9ec296710afe
mxd <838425805@qq.com>
2021-07-17 13:57:13 +0800

    去除无用代码

--- src/main/java/org/ssssssss/script/runtime/MagicScriptClassLoader.java
+++ src/main/java/org/ssssssss/script/runtime/MagicScriptClassLoader.java
@@ -1,12 +1,5 @@
 package org.ssssssss.script.runtime;
 
-import org.ssssssss.script.compile.MagicScriptCompiler;
-import org.ssssssss.script.MagicScript;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-
 public class MagicScriptClassLoader extends ClassLoader{
 
 

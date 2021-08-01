519c9394954dcd5681ccdfb4227501d93e7059e6
mxd <838425805@qq.com>
2021-08-01 19:44:53 +0800

    去除无用代码

--- src/main/java/org/ssssssss/script/runtime/MagicScriptClassLoader.java
+++ src/main/java/org/ssssssss/script/runtime/MagicScriptClassLoader.java
@@ -9,11 +9,6 @@ public class MagicScriptClassLoader extends ClassLoader{
 
 	public synchronized Class<MagicScriptRuntime> load(String className, byte[] bytecode) throws ClassNotFoundException {
 		defineClass(className, bytecode, 0 , bytecode.length);
-		try {
-			Files.write(Paths.get("E:\\idea\\MagicAPI\\magic-script\\src\\test\\java\\org\\ssssssss\\script","bytecode.class"), bytecode);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
 		return (Class<MagicScriptRuntime>) loadClass(className);
 	}
 }
